4c79b8f5f25855b314d8a5658fda67d93e074fb1
TimofeyMsk <timofeybelov@gmail.com>
2026-05-27 22:17:54 +0300

    Modified Macos keymap

--- keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/TimofeyMsk/keymap.c
+++ keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/TimofeyMsk/keymap.c
@@ -56,9 +56,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [SERVICE] = LAYOUT_69_ansi(
         _______,   KC_BRID,  KC_BRIU,  KC_TASK, KC_FILE, RGB_VAD, RGB_VAI, 				KC_MPRV,  KC_MPLY, KC_MNXT, KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,           				      _______,
-        RGB_TOG,   RGB_MOD,  RGB_VAI,  RGB_HUI, RGB_SAI, RGB_SPI,  		     			BT_HST1,  BT_HST2, BT_HST3,   P2P4G,  _______,  _______,  _______,  _______,                      _______,
+        RGB_TOG,   RGB_MOD,  RGB_VAI,  RGB_HUI, RGB_SAI, RGB_SPI,  		     			_______,  _______, _______, _______,  _______,  _______,  _______,  _______,                      _______,
         _______,   RGB_RMOD, RGB_VAD,  RGB_HUD, RGB_SAD, RGB_SPD,          				_______,  _______, _______, _______,  _______,  _______,  _______,                                DM_PLY1,
-        _______,             _______,  _______, _______, _______, BAT_LVL,  			BAT_LVL,  NK_TOGG, _______, _______,  _______,  _______,  _______, 		             DM_REC1,
+        _______,             BT_HST1,  BT_HST2, BT_HST3,   P2P4G, BAT_LVL,  			BAT_LVL,  NK_TOGG, _______, _______,  _______,  _______,  _______, 		             DM_REC1,
         _______, _______,  _______,          _______,             _______,  				_______,           _______,           			_______,   			    DM_RSTP, DM_REC2,     DM_PLY2),
 		
     [MAC_FN1] = LAYOUT_69_ansi(
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  KC_NO, KC_QUOT,  KC_MINS, KC_EQL,  KC_NO,  	     KC_HOME,LOPT(KC_LEFT),KC_UP, LOPT(KC_RGHT), KC_PGUP,  KC_NO,  KC_NO,  KC_NO,                                    KC_INS,
         _______,  KC_COLN, KC_LPRN,  KC_RPRN, KC_UNDS,  KC_NO,     			LOPT(KC_BSPC), KC_LEFT,KC_DOWN,KC_RGHT,KC_END, KC_PGDN,  KC_NO,                         				  KC_END,
         _______,     LCMD(KC_Z),LCMD(KC_X),LCMD(KC_C),LCMD(KC_V), KC_GRV,         KC_BSPC, KC_DEL , KC_LBRC,  KC_RBRC, KC_NO,   LCMD(KC_SLSH),  KC_NO,                     KC_PGUP,
-_______, _______,  _______,           _______,           _______,  	      KC_BSPC,           _______,                _______,                   	    KC_HOME, KC_PGDN,     KC_END)
+	    _______, _______,  _______,           KC_LSFT,           LCMD(KC_SPC),  	      LCMD(KC_SPC),           KC_RSFT,                _______,                   	    KC_HOME, KC_PGDN,     KC_END)
 };
 
 #if defined(ENCODER_MAP_ENABLE)
